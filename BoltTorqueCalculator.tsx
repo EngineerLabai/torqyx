8268615b1f42f198c3082bd837610213afb16519
EngineerLabai <engineerslabai@gmail.com>
2026-04-27 23:43:11 +0300

    fix: BoltTorqueCalculator içindeki UnitDisplay import hatası düzeltildi

--- BoltTorqueCalculator.tsx
+++ BoltTorqueCalculator.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useMemo } from "react";
-import UnitDisplay from "@/components/units/UnitDisplay";
+import { UnitDisplay } from "@/components/units/UnitDisplay";
 
 // --- STANDARD ENGINEERING DATA ---
 
